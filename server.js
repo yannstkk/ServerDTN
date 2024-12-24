c8bbd473c445e2f350c393d2cc8801db51413fbb
Yann <160952632+yannstkk@users.noreply.github.com>
2024-12-24 16:28:50 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -6,24 +6,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// Variable pour stocker les résultats du scan
 let scanResults = [];
 
-// Endpoint pour déclencher un scan (ne fait qu'envoyer une confirmation)
-app.post('/scan', (req, res) => {
-    console.log("Scan déclenché.");
-    res.status(200).json({ message: "Scan en cours, résultats attendus." });
-});
-
-// Endpoint pour recevoir les résultats envoyés par Flask
 app.post('/results', (req, res) => {
     try {
-        console.log("Données reçues : ", req.body);
+        const { data } = req.body;  
 
-        const { data } = req.body; 
         if (data && Array.isArray(data)) {
-            scanResults.push(...data); // Ajouter les nouveaux résultats à la liste existante
-            res.status(200).json({ message: "Résultats reçus avec succès" });
+            scanResults = data; 
+            console.log("Résultats mis à jour :", scanResults);
+            res.status(200).json({ message: "Résultats reçus et mis à jour." });
         } else {
             res.status(400).json({ error: "Format de données invalide" });
         }
@@ -33,12 +25,16 @@ app.post('/results', (req, res) => {
     }
 });
 
-// Endpoint de test pour vérifier que le serveur fonctionne
+
+app.get('/scan-results', (req, res) => {
+    res.json(scanResults);
+});
+
+
 app.get('/', (req, res) => {
     res.send('Serveur Express opérationnel !');
 });
 
-// Lancement du serveur
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
 });
